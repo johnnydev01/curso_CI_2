143f01786b6edea87c709fd2f56f1033df87efb8
Johnny Silva <johnnysilva.dev@gmail.com>
2025-05-29 16:42:33 -0300

    Corrigindo string de conexão com o banco de dados

--- database/db.go
+++ database/db.go
@@ -20,7 +20,15 @@ func ConectaComBancoDeDados() {
 	nomeBanco := os.Getenv("DB_NAME")
 	portaBanco := os.Getenv("DB_PORT")
 
-	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
+	if endereco == "" || usuario == "" || senha == "" || nomeBanco == "" || porta == "" {
+		panic("Uma ou mais variáveis de ambiente do banco de dados estão vazias")
+	}
+
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		endereco, usuario, senha, nomeBanco, porta,
+	)
+	
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
