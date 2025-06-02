ee0780b213262e0149f2aebac3bbd4554e47e812
Johnny Silva <johnnysilva.dev@gmail.com>
2025-06-02 09:36:17 -0300

    CI-CD EC2 (#2)
    * Configurando EC2 para Entrega Contínua
    
    * Evitando que a sessão SSH seja finalizada
    
    * Corrigindo sinxtaxe de branch
    
    * Configurando acesso local para o db
    
    * Corrigindo string de conexão com o banco de dados
    
    * Corrigindo nome da variável de porta do db
    
    * Corrigindo erro de variável undefined
    
    * Configurando nohup
    
    * Adicionando verificação na criação do binário
    
    * Corrigindo extenção do binário main
    
    * feat: Entrega Continua com Docker e EC2 e Github Actions
    
    * Alterando GitHub Actions para gerar binário Linux
    
    * Corrigindo deploy no workflow
    
    * Removendo .exe do binário gerado no build
    
    * Removendo binário main.exe do repositório e adiciona .gitignore
    
    * Deletando main.exe
    
    * Garantindo que residos de main.exe seja removido do ec2
    
    * Listando conteúdo do artefato baixado
    
    * Removendo passo de sobrescrita do artefato baixado
    
    * Mudando deploy ssh do EC2
    
    * Corrigindo diretório do binário
    
    * Movendo binário para a pasta raiz
    
    * Mudando diretório do binário baixado
    
    * Corrigindo dirtório scp
    
    * Incluindo caminho do binário
    
    * Corrigindo caminho do binário
    
    * Corrigindo identação da listage do coteúdo do artefato baixado
    
    * Removendo comando rm main
    
    * Corrigindo comando pkill main
    
    * Removendo comando pkill
    
    * Adicionando ssl mode
    
    * Criando variável de ambiente para o modo ssl
    
    * Adicionando virgula na string de conexão com o db
    
    * Corrigindo variáveis de ambies ec2

--- database/db.go
+++ database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"fmt"
 	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
@@ -19,8 +20,16 @@ func ConectaComBancoDeDados() {
 	senha := os.Getenv("DB_PASSWORD")
 	nomeBanco := os.Getenv("DB_NAME")
 	portaBanco := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("DB_SSLMODE")
 
-	stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
+	if endereco == "" || usuario == "" || senha == "" || nomeBanco == "" || portaBanco == "" || sslMode == ""{
+		panic("Uma ou mais variáveis de ambiente do banco de dados estão vazias")
+	}
+
+	stringDeConexao := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		endereco, usuario, senha, nomeBanco, portaBanco, sslMode)
+
+
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
